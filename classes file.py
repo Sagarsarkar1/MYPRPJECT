db3fa489af4faf29c075d9f4744f53ad623035a4
Sagarsarkar1 <110330463+Sagarsarkar1@users.noreply.github.com>
2022-08-05 10:05:09 +0530

    Add files via upload

---
+++ classes file.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import os
+
+
+# In[7]:
+
+
+os. getcwd()
+
+
+# In[29]:
+
+
+pd()
+
+
+# In[30]:
+
+
+import pandas as pd
+
+
+# In[32]:
+
+
+pd.read_csv("stu.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import pandas as pd 
+
+
+# In[36]:
+
+
+pd.read_csv("stu.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
